1e053ac3a2b8f604124fc68d8f21a0a5bd1b55b5
Sidharth <sidharthghai4242@gmail.com>
2023-08-28 14:29:34 +0530

    modified files //3

---
+++ lib/pages/nav_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:rlr/pages/activity_page.dart';
+import 'package:rlr/pages/profile_page.dart';
+import 'package:rlr/pages/mybooks_page.dart';
+import 'package:rlr/pages/home_screen.dart';
+import 'package:rlr/pages/search_page.dart';
+import 'package:flutter/cupertino.dart';
+
+
+class NavPage extends StatefulWidget {
+  const NavPage({Key? key}) : super(key: key);
+
+  @override
+  State<NavPage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<NavPage> {
+  int _currentIndex = 0; // Index of the selected bottom navigation bar item
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    SearchPage(),
+    MyBooksPage(),
+    ActivityPage(),
+    ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(8),
+        child: Container(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(180),
+              color: Color(0xFFFFDBD1),
+            ),
+
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+              child: GNav(
+                backgroundColor: Color(0xFFFFDBD1),
+                color: Color(0xFF3B0900),
+                activeColor: Color(0xFF3B0900),
+                tabBackgroundColor: Color(0xFF9B442B),
+                gap: 8,
+                padding: EdgeInsets.all(10),
+                tabs: [
+                  GButton(icon: Icons.home,text: 'Home',),
+                  GButton(icon: Icons.search, text: 'Search'),
+                  GButton(icon: Icons.book, text: 'My Books'),
+                  GButton(icon: Icons.auto_graph_sharp,text: 'My Activity',),
+                  GButton(icon: Icons.person,text: 'My Profile',),
+                ],
+                selectedIndex: _currentIndex,
+                onTabChange: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
